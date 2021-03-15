efc6315aac3bb5c65ee5017cad66938ca40ad5ff
shobir <soberirsan13@gmail.com>
2021-03-15 13:38:37 +0700

    change your place and your api

--- lib/api_openWeather.dart
+++ lib/api_openWeather.dart
@@ -1,6 +1,6 @@
 import 'package:http/http.dart' as http;
 class ApiOpenWheather{
-  String _url  = "http://api.openweathermap.org/data/2.5/weather?q=Medan&appid=814ecc4782b770546f968dd8a8c3f571";
+  String _url  = "http://api.openweathermap.org/data/2.5/weather?q=YourPlace&appid=yourApi";
   Future getWeather()async{
     return await http.get(_url);
   }
